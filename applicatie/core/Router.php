3afe7906e84da4bbae3959094b4d6254b6168956
Siem Pouwels <spg.pouwels@student.han.nl>
2025-04-15 22:31:21 +0200

    Improved router

--- applicatie/core/Router.php
+++ applicatie/core/Router.php
@@ -20,33 +20,35 @@ class Router
 
     public function dispatch(): void
     {
-        $requestUri = strtok(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '?');
-        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        $uri = strtok(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '?');
+        $method = $_SERVER['REQUEST_METHOD'];
+        $routesForMethod = $this->routes[$method] ?? [];
 
-        // Check op exacte match
-        if (isset($this->routes[$requestMethod][$requestUri])) {
-            $handler = $this->routes[$requestMethod][$requestUri];
-            call_user_func($handler);
+        // Exact match
+        if (isset($routesForMethod[$uri])) {
+            call_user_func($routesForMethod[$uri]);
             return;
         }
 
-        // Dynamische route matching (bv. /product/{naam})
-        $definedRoutes = $this->routes[$requestMethod] ?? [];
+        // Dynamic routes
+        foreach ($routesForMethod as $route => $handler) {
+            $regex = preg_replace('#\{[^/]+\}#', '([^/]+)', $route);
+            $pattern = "#^{$regex}$#";
 
-        foreach ($definedRoutes as $routeDefinition => $handler) {
-            // Vervang {param} door een regex-matchgroep (zoals ([^/]+))
-            $regexifiedRoute = preg_replace('#\{[^/]+\}#', '([^/]+)', $routeDefinition);
-            $regexPattern = "#^{$regexifiedRoute}$#";
-
-            if (preg_match($regexPattern, $requestUri, $matches)) {
-                array_shift($matches); // verwijder volledige match uit de resultaten
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches); // remove full match
                 call_user_func_array($handler, $matches);
                 return;
             }
         }
 
-        // Geen match gevonden
+        // No route matched
+        $this->handleNotFound();
+    }
+
+    private function handleNotFound(): void
+    {
         http_response_code(404);
-        include __DIR__.'/../views/error/404.php'; 
+        include __DIR__.'/../views/error/404.php';
     }
 }
